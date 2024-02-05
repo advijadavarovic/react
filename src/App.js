3a72246d789469c7bbf85865ae731ea1f97e8599
advijadavarovic <advija.davarovic@fet.ba>
2024-02-05 09:49:15 +0100

    not finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {lazy, Suspense} from "react";
+import LinearProgress from '@mui/material/LinearProgress';
+import {useAuthContext} from "./context/AuthContext";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+
+const LazyRootLayout = lazy(() => import( './pages/Root'));
+const LazySignUp = lazy(() => import( './pages/SignUp'));
+const LazyLogin = lazy(() => import( './pages/Login'));
+const LazyDashboardPage = lazy(() => import( './pages/MainDashboard'));
+const LazyProfilePage = lazy(() => import('./pages/Profile'));
 function App() {
+  const user = useAuthContext();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Suspense fallback={<LinearProgress color="secondary" />}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LazyRootLayout />} />
+              <Route path="signup" element={<LazySignUp />} />
+              <Route path="login" element={<LazyLogin />} />
+              <Route
+                  path="dashboard"
+                  element={user ? <LazyDashboardPage /> : <LazyLogin />}
+              />
+              <Route
+                  path="profile"
+                  element={user ? <LazyProfilePage /> : <LazyLogin/> }
+              />
+              <Route path="*" element={user ? <LazyDashboardPage /> : <LazyRootLayout/> } />
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
   );
 }
-
 export default App;
